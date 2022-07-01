8ebaaa2d01c650138efb2d25f3a1a913e9c6a5e4
test <106772162+ExploitEngine1@users.noreply.github.com>
2022-07-01 18:13:05 +0200

    Update script.js

--- client/script.js
+++ client/script.js
@@ -2,7 +2,7 @@
 var script = document.createElement('script');
 script.src = "https://code.jquery.com/jquery-3.4.1.min.js";
 document.getElementsByTagName('head')[0].appendChild(script);
-const socket = new WebSocket('wss://FrillyUntrueModel.game-production.repl.co/');
+const socket = new WebSocket('wss://REPLIT-URL-HERE'); // YOUR REPLIT URL OR IP HERE
 socket.addEventListener('open', function (event) {
  console.log('Connected to server')
  t = setInterval(function() {
